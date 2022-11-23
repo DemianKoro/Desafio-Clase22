f04e2ae300962b7b6d081da53d4effd934410f3f
Demian <demian.koro@gmail.com>
2022-11-23 19:04:24 -0300

    fixing submodule error

---
+++ src/database/mysql/products.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    id: '04cd10cf-5e53-46b7-9074-7da2aed162cd',
+    name: 'Wild Cards',
+    price: 979,
+    url: 'https://cdn3.iconfinder.com/data/icons/fantasy-and-role-play-game-adventure-quest/512/Fairy-512.png',
+  },
+  {
+    id: '2b6c0064-4158-4107-a5d0-0e4f32c28e67',
+    name: 'Drácula',
+    price: 890,
+    url: 'https://cdn3.iconfinder.com/data/icons/font-awesome-solid/512/book-quran-512.png',
+  },
+  {
+    id: '549ebde7-d12b-4c76-896b-618a106cbf7b',
+    name: 'Short Rings',
+    price: 1450,
+    url: 'https://cdn3.iconfinder.com/data/icons/font-awesome-solid/512/book-quran-512.png',
+  },
+
+];
+
+module.exports = { products }; 
